1eef3674d522ba98cf92b399fa248df03adbb329
Dann Stockton <dannstockton@gmail.com>
2023-06-11 09:21:22 -0600

    Add default font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+      '6xl': '5.052rem',
+    },
     extend: {
       fontFamily: {
         baskervvile: ['var(--font-baskervvile)'],
