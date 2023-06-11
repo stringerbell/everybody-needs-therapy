c028b7cf4f1d1ef9c03dc6b7d4883f47c25e2967
Dann Stockton <dannstockton@gmail.com>
2023-06-11 08:38:53 -0600

    Try and deploy from github actions
    https://www.viget.com/articles/host-build-and-deploy-next-js-projects-on-github-pages/

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
 }
 
-module.exports = nextConfig
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
